64edb7e418ffa1f84ac58a95794f1ae26ec18033
swasthik <swastiknj76@gmail.com>
2025-08-05 00:08:15 +0530

    done with langing page

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -1,16 +1,41 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 import { Link } from "react-router-dom";
 import profilePicture from "/_DSC0069.jpg";
+import { FaMoon, FaSun } from "react-icons/fa";
 
 export default function Home() {
+  const [darkMode, setDarkMode] = useState(false);
   const change = () => {
     document.documentElement.classList.toggle("dark");
+    setDarkMode(!darkMode);
   };
+useEffect(() => {
+    const isDark = document.documentElement.classList.contains("dark");
+    setDarkMode(isDark);
+  }, []);
+  
   return (
     <div className="w-full h-screen bg-white dark:bg-black transition-colors duration-700">
-      <button className="text-black dark:text-white" onClick={change}>
-        click
-      </button>
+      <div className=" flex items-center gap-5 absolute top-4 right-5 sm:right-14 flex-col sm:flex-row">
+        <button
+          onClick={change}
+          className=" text-xl p-2 rounded-full cursor-pointer text-black dark:text-white hover:bg-gray-200 dark:hover:bg-gray-800 transition"
+        >
+          {darkMode ? (
+            <span class="material-symbols-outlined">light_mode</span>
+          ) : (
+            <span class="material-symbols-outlined">dark_mode</span>
+          )}
+        </button>
+        <Link
+          to="/explore"
+          className="hidden sm:flex w-48 text-center px-6 py-2.5 rounded-3xl border bg-white text-black dark:bg-white dark:text-black transition items-center justify-center gap-2"
+        >
+          More About
+          <span className="material-symbols-outlined">arrow_outward</span>
+        </Link>
+      </div>
+
       <div className="w-full max-w-[70rem] mx-auto  min-h-screen justify-center items-center flex flex-col gap-6 sm:px-6 lg:px-8">
         <div className="items-center flex flex-col">
           <img
@@ -38,15 +63,20 @@ export default function Home() {
         <div className="flex flex-col sm:flex-row gap-4 justify-center items-center">
           <Link
             to="/explore"
-            className="w-48 text-center px-6 py-2.5 rounded-3xl bg-black text-white dark:bg-white dark:text-black transition"
+            className="w-48 text-center px-6 py-2.5 rounded-3xl bg-black text-white dark:bg-white dark:text-black transition flex items-center justify-center gap-2"
           >
             More About
+            <span class="material-symbols-outlined">
+arrow_forward
+</span>
           </Link>
           <Link
-            to="/resume"
-            className="w-48 text-center px-6 py-2.5 rounded-3xl border text-black dark:text-white border-black dark:border-white transition"
+            to="https://tinyurl.com/swasthiknj-resume"
+            target="_blank"
+            className="w-48 text-center px-6 py-2.5 rounded-3xl border text-black dark:text-white border-black dark:border-white transition flex items-center justify-center gap-2"
           >
             Resume
+            <span className="material-symbols-outlined">download</span>
           </Link>
         </div>
       </div>
