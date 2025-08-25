337de81f94a68363975cab210065fdabd19021f5
swasthik <swastiknj76@gmail.com>
2025-08-25 23:40:08 +0530

    updating project page

--- src/pages/Project.jsx
+++ src/pages/Project.jsx
@@ -7,8 +7,28 @@ export default function Project() {
         Projects
         <span className="absolute left-0 -bottom-1 h-[6px] w-14 bg-orange-500 rounded-md"></span>
       </h1>
-      <div>
-      <h1 className='text-white'>Coming Soon...</h1>
+      <div className='flex gap-5 justify-center flex-wrap'>
+        <div className=' p-2 bg-amber-100 w-fit'>
+          <div className='h-[8rem] md:w-[15rem]   w-[18rem]   bg-amber-400 rounded-md projects'>imge</div>
+          <div>
+            <h1>Project Title</h1>
+            <p>Project Description</p>
+          </div>
+        </div>
+        <div className=' p-2 bg-amber-100 w-fit'>
+          <div className='h-[8rem] md:w-[15rem]   w-[18rem]   bg-amber-400 rounded-md projects'>imge</div>
+          <div>
+            <h1>Project Title</h1>
+            <p>Project Description</p>
+          </div>
+        </div>
+        <div className=' p-2 bg-amber-100 w-fit'>
+          <div className='h-[8rem] md:w-[15rem]   w-[18rem]  bg-amber-400  rounded-md projects'>imge</div>
+          <div>
+            <h1>Project Title</h1>
+            <p>Project Description</p>
+          </div>
+        </div>
       </div>
     </div>
   )
