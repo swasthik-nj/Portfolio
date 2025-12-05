07b14e0e92f0150487ebc0b6081755d0770ab3b4
swasthik <swastiknj76@gmail.com>
2025-12-06 00:15:02 +0530

    initial commit

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -1,6 +1,6 @@
 import React, { useEffect, useState } from "react";
 import { Link } from "react-router-dom";
-import profilePicture from "/Swasthik.jpg";
+import profilePicture from "/swanj.jpeg";
 
 export default function Home() {
   
